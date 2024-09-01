b71c610a4340ad3e61514f45715d140f28b43758
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-01 11:36:02 -0500

    updates to singular contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,12 +2,14 @@ class ContactsController < ApplicationController
   def first_contact
     contact = Contact.find_by(id: 1)
     render json: {
-      contacts: contact
+      id: contact.id,
+      First_name: contact.first_name, 
+      Last_name: contact.last_name, 
+      Email: contact.email
   }
   end 
   def all
     contacts = Contact.all
-    p contacts 
     render json: { contacts: contacts }
   end
 end
