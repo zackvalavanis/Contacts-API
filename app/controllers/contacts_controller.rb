f9be03f994e23717e5e55bb064afc467c2ab45fe
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-01 12:59:26 -0500

    last

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,15 +8,3 @@ class ContactsController < ApplicationController
     render template: "contacts/index"
   end
 end
-
-class ContactsController < ApplicationController
-  def first_contact
-    @contact = Contact.find_by(id: 1)
-    render template: "contacts/show"
-  end
-
-  def all
-    @contacts = Contact.all
-    render template: "contacts/index"
-  end
-end
