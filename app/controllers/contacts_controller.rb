db75dc4544f5cb04b2b2e506932c80c817172c0e
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-01 12:52:54 -0500

    contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,22 @@
 class ContactsController < ApplicationController
   def first_contact
-    contact = Contact.find_by(id: 1)
-    render json: {
-      id: contact.id,
-      First_name: contact.first_name, 
-      Last_name: contact.last_name, 
-      Email: contact.email
-  }
-  end 
+    @contact = Contact.find_by(id: 1)
+    render template: "contacts/show"
+  end
   def all
-    contacts = Contact.all
-    render json: { contacts: contacts }
+    @contacts = Contact.all
+    render template: "contacts/index"
+  end
+end
+
+class ContactsController < ApplicationController
+  def first_contact
+    @contact = Contact.find_by(id: 1)
+    render template: "contacts/show"
+  end
+
+  def all
+    @contacts = Contact.all
+    render template: "contacts/index"
   end
 end
---
+++ app/views/contacts/_contact.json.jbuilder
@@ -0,0 +1,4 @@
+json.id @contact.id
+json.first_name @contact.first_name
+json.last_name @contact.last_name
+json.email @contact.email
